8859df68ebc95fc5a7b9a8df1442d3695d43229c
Sebastian Belczyk <sbelczyk@gmail.com>
2015-01-14 23:22:51 +1300

    add lexing

---
+++ src/ElasticOps.Parsing/Tokens.fs
@@ -0,0 +1,16 @@
+﻿module Tokens
+open System 
+
+type Tokens = 
+    | INT of int
+    | DECIMAL of decimal
+    | STRING of String
+    | BOOLEAN of bool
+    | L_S_BRAC 
+    | R_S_BRAC
+    | L_C_BRAC
+    | R_C_BRAC
+    | SEMICOLON
+    | EOF
+
+    
