64230b84ad71f9afb689c707349600d8d37deb53
Sebastian Belczyk <sbelczyk@gmail.com>
2015-01-25 20:41:11 +1300

    extend grammar to parse partial json

--- src/ElasticOps.Parsing.Tests/ParsingTests.fs
+++ src/ElasticOps.Parsing.Tests/ParsingTests.fs
@@ -11,7 +11,6 @@ let countSubstring (where :string) (what : string) =
     | "" -> 0 // just a definition; infinity is not an int
     | _ -> (where.Length - where.Replace(what, @"").Length) / what.Length
 
-
 [<Test>]
 let ``parse values ``() =
     "true"  |> parse |> should equal (Some(JsonValue.Bool(true)))
@@ -23,13 +22,26 @@ let ``parse values ``() =
     "\"abc\"" |> parse |> should equal (Some(JsonValue.String("abc")))
 
 [<Test>]
+let ``parse partial true value``() = 
+    "t"  |> parse |> should equal (Some(JsonValue.Bool(true)))
+    "tr"  |> parse |> should equal (Some(JsonValue.Bool(true)))
+    "tru"  |> parse |> should equal (Some(JsonValue.Bool(true)))
+
+[<Test>]
+let ``parse partial false value``() = 
+    "f"  |> parse |> should equal (Some(JsonValue.Bool(false)))
+    "fa"  |> parse |> should equal (Some(JsonValue.Bool(false)))
+    "fal"  |> parse |> should equal (Some(JsonValue.Bool(false)))
+    "fals"  |> parse |> should equal (Some(JsonValue.Bool(false)))
+
+[<Test>]
 let ``parse simple objects`` () =
     //no peroperties
     "{}" |> parse |> should equal (Some(JsonValue.Assoc([])))
     //one property object
     "{ \"prop\" : 1.25 }" |> parse |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Float(1.25))])))
     //multiple properties
-    "{ \"prop\" : 12, \"prop2\" : \"123\" }" |> parse |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Int(12));("prop2",JsonValue.String("123"))])))
+    "{ \"prop\" : +12, \"prop2\" : \"123\" }" |> parse |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Int(12));("prop2",JsonValue.String("123"))])))
     //property of type array 
     "{ \"prop\" : [1,23] }" |> parse |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.List([JsonValue.Int(1); JsonValue.Int(23)]))])))
 
@@ -78,11 +90,103 @@ let ``parse random complex json without parse error`` () =
 
     json |> parse |> ignore
 
-
 [<Test>]
-[<Ignore>]
-let ``can parse not complete json`` () = 
-    let json = "{ \"prop\" : 1 "
+let ``can parse json terminated before ending }`` () = 
+    let json = "{ \"prop\" : 1"
     let res = json |> parse
 
-    res |> should equal (JsonValue.Assoc([("prop",JsonValue.Int(1))]))
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Int(1))])))
+
+[<Test>]
+let ``can parse json ending with colon`` () = 
+    let json = "{ \"prop\" : "
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Colon)])))
+    
+[<Test>]
+let ``can parse json after property name`` () = 
+    let json = "{ \"prop\" "
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.Missing)])))
+
+[<Test>]
+let ``can parse json terminated in the middle of array`` () = 
+    let json = "{ \"prop\" : [ 1,2 "
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.List([JsonValue.Int(1);JsonValue.Int(2)]))])))
+
+[<Test>]
+let ``can parse json terminated in the middle of array with colon`` () = 
+    let json = "{ \"prop\" : [ 1,2, "
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.List([JsonValue.Int(1);JsonValue.Int(2)]))])))
+
+[<Test>]
+let ``can parse json terminated in the middle with incomplete object`` () = 
+    let json = "{ \"prop\" : [ { \"x\"  "
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("prop",JsonValue.List([JsonValue.Assoc([("x",JsonValue.Missing)])]))])))
+
+[<Test>]
+let ``parser json ending with object terminated with comma after full property`` () = 
+    let json = "[{\"id\": 1,\"name\": \"Kathie Steele\"},{\"id\": 2,"
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.List([
+        JsonValue.Assoc([
+            ("id", JsonValue.Int(1));
+            ("name", JsonValue.String("Kathie Steele"))
+        ]);
+        JsonValue.Assoc([
+            ("id", JsonValue.Int(2))
+        ])
+    ])))
+
+[<Test>]
+let ``can parse json terminated in the middle of property name`` () = 
+    let json = "{ \"pro"
+    let res = json |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("pro",JsonValue.Missing)])))
+
+[<Test>]
+let ``can parse json terminated after colon`` () = 
+    let json = "{ \"pro\" :"
+    let json2 = "{ \"pro\" : "
+    let json3 = "{ \"pro\":"
+    let json4 = "{ \"p\" : 1, \"pro\":"
+    let res = json |> parse
+    let res2 = json2 |> parse
+    let res3 = json3 |> parse
+    let res4 = json4 |> parse
+
+    res |> should equal (Some(JsonValue.Assoc([("pro",JsonValue.Colon)])))
+    res2 |> should equal (Some(JsonValue.Assoc([("pro",JsonValue.Colon)])))
+    res3 |> should equal (Some(JsonValue.Assoc([("pro",JsonValue.Colon)])))
+    res4 |> should equal (Some(JsonValue.Assoc([("p",JsonValue.Int(1));("pro",JsonValue.Colon)])))
+
+
+[<Test>]
+let ``can parse json terminated in a random place`` () = 
+    let rnd = System.Random(System.DateTime.Now.Millisecond)
+    let json = System.IO.File.ReadAllText "randomComplexTestsJson.json"
+    let lines = json.Split('\n') 
+    let lineCount = lines |> Seq.length
+
+    for i in [1..1000] do
+        let lineNum = rnd.Next(2,lineCount)
+        let line = Seq.nth lineNum lines
+        let lineLen = String.length line
+        let columnNum = rnd.Next(0,lineLen)
+        let fullLines = lines |> Seq.take (lineNum-1)
+        let json = (String.concat "\n" fullLines) + line.Substring(0,columnNum)
+
+        try 
+            json |> parse  |> ignore
+        with 
+        | e -> Assert.Fail ("Parser error for JSON: " + json)
--- src/ElasticOps.Parsing/Structures.fs
+++ src/ElasticOps.Parsing/Structures.fs
@@ -8,3 +8,25 @@ type JsonValue =
   | List of JsonValue list
   | Null
   | String of string
+  | Missing
+  | Colon
+
+
+  //below function is not important, it simply prints values 
+  override x.ToString() = 
+            match x with
+            | Bool b -> sprintf "Bool(%b)" b
+            | Float f -> sprintf "Float(%f)" f
+            | Int d -> sprintf "Int(%d)" d
+            | String s -> sprintf "String(%s)" s
+            | Null ->  "Null()"
+            | Colon -> "':'"
+            | Missing -> "/missing/"
+            | Assoc props ->  props 
+                               |> List.map (fun (name,value) -> sprintf "\"%s\" : %s" name (value.ToString())) 
+                               |> String.concat ","
+                               |> sprintf "Assoc(%s)"
+            | List values ->  values
+                               |> List.map (fun value -> value.ToString()) 
+                               |> String.concat ","
+                               |> sprintf "List(%s)"
