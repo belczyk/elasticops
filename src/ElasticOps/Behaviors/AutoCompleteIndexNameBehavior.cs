27ec0c08903cfa92e2b1cff9e2fb40e96d79e8d1
Sebastian Belczyk <sbelczyk@gmail.com>
2015-01-28 23:34:01 +1300

    autocomplete behavior

---
+++ src/ElasticOps/Behaviors/AutoCompleteIndexNameBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ElasticOps.Behaviors
+{
+    public class DragBehavior : Behavior<AutoCompleteBox>
+    {
+        protected override void OnAttached()
+        {
+
+        }
+    }
+}
