235a62eea6e6380cd063940ff13adf04b44110df
belczyk <sbelczyk@gmail.com>
2015-02-04 11:31:23 +1300

    fix problems with string till position function

--- src/ElasticOps.Intellisense/String.fs
+++ src/ElasticOps.Intellisense/String.fs
@@ -1,10 +1,11 @@
 ﻿[<AutoOpen>]
 module Microsoft.FSharp.Core.String
+open System
 
 let substring (str : string) (line : int) col = 
-    match str.Replace("\r\n","\n").Replace("\r","\n").Split('\n') |> List.ofArray with
+    match str.Split([|"\r\n"|], StringSplitOptions.None) |> List.ofArray with
     | l::[] -> str.Substring(0,col-1)
     | lines -> let leadingLines = lines|> Seq.ofList |> Seq.take (line - 1) |> List.ofSeq
                let lastLine = (Seq.nth (line - 1) lines)
-               (leadingLines |> String.concat "\n")+lastLine
+               (leadingLines |> String.concat "\r\n")+lastLine
 
