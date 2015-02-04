b181481db0c6d69d06b2620306bc77ee20b243f9
Sebastian Belczyk <sbelczyk@gmail.com>
2015-02-04 18:07:41 +1300

    fix problems with FSharp.Core

---
+++ src/ElasticOps.Intellisense.Tests/DSLPathTests.fs
@@ -0,0 +1,17 @@
+﻿module DSLPathTests
+
+open NUnit.Framework
+open FsUnit
+open ElasticOps.Parsing.Processing
+open ElasticOps.Parsing.Structures
+open ElasticOps
+open ElasticOps.DSLPath
+
+[<Test>]
+let ``path for property in object`` () = 
+//    let res = @"{ ""prop"" : 1" 
+//                |> parse
+//                |> Option.get
+//                |> getDLSPath
+//    res |> should equal [Object;Property("prop");Value(JsonValue.Int(1))]
+    1 |> should equal 1
