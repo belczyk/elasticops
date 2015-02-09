af6e5ab0b5887fbe4feae61d71d4700f192b1711
belczyk <sbelczyk@gmail.com>
2015-02-10 12:22:50 +1300

    Selectedindex dependency property for AutoCompleteBox and filter autocomplete

---
+++ src/ElasticOps/DependencyProperties/AutoCompleteBoxProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ElasticOps.DependencyProperties
+{
+    public class AutoCompleteBoxProperties : DependencyObject
+    {
+        public static DependencyProperty SelectedIndexProperty = DependencyProperty.RegisterAttached("SelectedIndex", typeof(string), typeof(AutoCompleteBox));
+
+        public static bool GetSelectedIndex(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SelectedIndexProperty);
+        }
+
+        public static void SetSelectedIndex(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SelectedIndexProperty, value);
+        }
+
+    }
+}
